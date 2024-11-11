4bcfaac365477490d92c1729d9c807d3327d641d
hoooonshub <yhn515@naver.com>
2024-11-11 23:15:48 +0900

    fix: 출력 포맷 개행 추가

--- src/main/java/store/read/Reader.java
+++ src/main/java/store/read/Reader.java
@@ -114,19 +114,19 @@ public class Reader {
     }
 
     public static String readAgreeReceivingFree(String productName, int quantity) {
-        System.out.println("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)".formatted(productName, quantity));
+        System.out.println("\n현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)".formatted(productName, quantity));
 
         return inputYOrN();
     }
 
     public static String readAgreeRegularPrice(String productName, int quantity) {
-        System.out.println("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)".formatted(productName, quantity));
+        System.out.println("\n현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)".formatted(productName, quantity));
 
         return inputYOrN();
     }
 
     public static String readApplyMembership() {
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        System.out.println("\n멤버십 할인을 받으시겠습니까? (Y/N)");
 
         return inputYOrN();
     }
