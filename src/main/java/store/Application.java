0401cf47a599eb38810efd45a36c6b203efb45f4
hoooonshub <yhn515@naver.com>
2024-11-11 18:28:58 +0900

    refactor: 파일 경로명 변경

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -12,8 +12,8 @@ import store.stock.Stock;
 import java.util.List;
 
 public class Application {
-    private static final String PROMOTION_FILE_PATH = "java-convenience-store-7-hoooonshub/src/main/resources/promotion.md";
-    private static final String PRODUCT_FILE_PATH = "java-convenience-store-7-hoooonshub/src/main/resources/products.md";
+    private static final String PROMOTION_FILE_PATH = "src/main/resources/promotions.md";
+    private static final String PRODUCT_FILE_PATH = "src/main/resources/products.md";
 
 
     public static void main(String[] args) {
